eba7f598f93d9ec865f4ae74951665158967d298
Antoine BENEY <antoine.beney@diiage.org>
2024-03-06 16:47:32 +0100

    Add event list component

--- src/app/create-event-form/create-event-form.component.ts
+++ src/app/create-event-form/create-event-form.component.ts
@@ -1,12 +1,31 @@
+// event-list.component.ts
+
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-create-event-form',
-  standalone: true,
-  imports: [],
-  templateUrl: './create-event-form.component.html',
-  styleUrl: './create-event-form.component.css'
+  selector: 'app-event-list',
+  templateUrl: './event-list.component.html',
+  styleUrls: ['./event-list.component.css']
 })
-export class CreateEventFormComponent {
-
+export class EventListComponent {
+  events: any[] = [
+    {
+      title: 'Événement 1',
+      description: 'Description de l\'événement 1.',
+      date: '2024-03-10',
+      location: 'Lieu de l\'événement 1'
+    },
+    {
+      title: 'Événement 2',
+      description: 'Description de l\'événement 2.',
+      date: '2024-03-15',
+      location: 'Lieu de l\'événement 2'
+    },
+    {
+      title: 'Événement 3',
+      description: 'Description de l\'événement 3.',
+      date: '2024-03-20',
+      location: 'Lieu de l\'événement 3'
+    }
+  ];
 }
